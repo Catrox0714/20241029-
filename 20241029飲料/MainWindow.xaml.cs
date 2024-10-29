d6353432d1cfd093a1d3bb5b768734f3135507fa
Catrox <4b2g0122@stust.edu.tw>
2024-10-29 22:55:33 +0800

    slider練習

--- 20241029飲料/MainWindow.xaml.cs
+++ 20241029飲料/MainWindow.xaml.cs
@@ -24,5 +24,28 @@ namespace _20241029飲料
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var targetTextBox = sender as TextBox;
+            int amount;
+            bool success=int.TryParse(targetTextBox.Text, out amount);
+            if (!success)
+            {
+                MessageBox.Show("請輸入正整數", "輸入錯誤");
+            }
+            else if (amount <= 0)
+            {
+                MessageBox.Show("請輸入正整數", "輸入錯誤");
+            }
+            else
+            {
+                var targetStackPanel = targetTextBox.Parent as StackPanel;
+                var targetLabel = targetStackPanel.Children[0] as Label;
+                var drinkName = targetLabel.Content.ToString();
+                //MessageBox.Show($"您選擇了{drinkName}，數量為{amount}杯", "訂購完成");
+                ResultTextBlock.Text += $"您選擇了{drinkName}，數量為{amount}杯\n";
+            }
+        }
     }
 }
